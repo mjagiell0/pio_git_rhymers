6f822f32eea75dd8ff1cedc9d6a6a83c2037906e
mjagi <m.jagiello03@gmail.com>
2024-06-03 14:58:57 +0200

    Dział 3. github

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,31 +3,61 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+/**
+ * The RhymersDemo class demonstrates the use of the rhymers from the factory.
+ */
 class RhymersDemo {
 
+    /**
+     * The main method which serves as the entry point of the program.
+     *
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-        
+        testRhymers(factory);
+    }
+
+    /**
+     * Tests the rhymers created by the given factory.
+     *
+     * @param factory The factory to create rhymers
+     */
+    private static void testRhymers(Rhymersfactory factory) {
+        DefaultCountingOutRhymer[] rhymers = {
+                factory.GetStandardRhymer(),
+                factory.GetFalseRhymer(),
+                factory.GetFIFORhymer(),
+                factory.GetHanoiRhymer()
+        };
+        /**
+         * Tests the rhymers created by the given factory.
+         */
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
-        
+
+        /**
+         *  Populate the HanoiRhymer with random values
+         */
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
-        
+
+        /**
+         *  Print the contents of each rhymer
+         */
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
+        /**
+         *  Print the total number of rejected values for the HanoiRhymer
+         */
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
     }
-    
+
 }
